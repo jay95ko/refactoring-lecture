9687125f83044cb6205fb67b4b720efc44ac4213
고준영 <kojang1995@gmail.com>
2022-07-02 16:09:56 +0900

    ADD: 6-1 함수 추출하기

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,27 +1,37 @@
-export function printOwing(invoice) {
-  let outstanding = 0;
-
+function printBanner() {
   console.log('***********************');
   console.log('**** Customer Owes ****');
   console.log('***********************');
+}
 
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+function calculateOutstanding(invoice) {
+  // eslint-disable-next-line no-return-assign
+  return invoice.orders.reduce((sum, order) => sum += order.amount, 0);
+}
 
-  // record due date
-  const today = new Date();
+function recordDueDate(invoice, today) {
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
-    today.getDate() + 30
+    today.getDate() + 30,
   );
+}
 
-  //print details
+function printDetails(invoice, outstanding) {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+export default function printOwing(invoice) {
+  printBanner();
+
+  const outstanding = calculateOutstanding(invoice);
+
+  const today = new Date();
+  recordDueDate(invoice, today);
+
+  printDetails(invoice, outstanding);
 }
 
 const invoice = {
