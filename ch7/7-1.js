b3118c8e574fea31ff18f26edb1c6918c453dd5b
고준영 <kojang1995@gmail.com>
2022-07-07 19:45:48 +0900

    ADD: 7-1 레코드 캡슐화하기

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,32 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+    #name;
+    #country;
+
+    constructor(name, country) {
+        this.#name = name;
+        this.#country = country;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get country() {
+        return this.#country;
+    }
+
+    set name(name) {
+        this.#name = name;
+    }
+
+    get rawData() {
+        return {name: this.#name, country: this.#country}
+    }
+}
+
+const organization = new Organization('Acme Gooseberries', 'GB');
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
 console.log(organization.country);
+console.log(organization.rawData);
