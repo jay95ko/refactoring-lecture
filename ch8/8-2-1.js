245050da586b015bcf07ee9fd283c0a1346f3f85
고준영 <kojang1995@gmail.com>
2022-07-16 14:05:33 +0900

    ADD: 8-2 필드 옮기기

--- ch8/8-2-1.js
+++ ch8/8-2-1.js
@@ -1,34 +1,34 @@
 export class Customer {
   #name;
-  #discountRate;
   #contract;
-  constructor(name, discountRate) {
+  constructor(name, contract) {
     this.#name = name;
+    this.#contract = contract;
+  }
+
+  becomePreferred() {
+    this.#contract.discountRate += 0.03;
+    // 다른 코드들이 있음...
+  }
+
+  applyDiscount(amount) {
+    return amount.subtract(amount.multiply(this.#contract.discountRate));
+  }
+}
+
+class CustomerContract {
+  #startDate;
+  #discountRate;
+  constructor(startDate, discountRate) {
     this.#discountRate = discountRate;
-    this.#contract = new CustomerContract(this.dateToday());
+    this.#startDate = startDate;
   }
 
   get discountRate() {
     return this.#discountRate;
   }
 
-  becomePreferred() {
-    this.#discountRate += 0.03;
-    // 다른 코드들이 있음...
-  }
-
-  applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this.#discountRate));
-  }
-
-  dateToday() {
-    return new Date();
-  }
-}
-
-class CustomerContract {
-  #startDate;
-  constructor(startDate) {
-    this.#startDate = startDate;
+  set discountRate(value) {
+    return this.#discountRate = value;
   }
 }
